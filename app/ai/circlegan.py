ad890cf734353d0ad58df7b4ff3c68f0b591ecfd
Crystal <lingchequ@gmail.com>
2022-12-06 15:33:01 +0800

    update GAN

--- app/ai/circlegan.py
+++ app/ai/circlegan.py
@@ -1,7 +1,7 @@
 import os
 from glob import glob
 import numpy as np
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 import tensorflow as tf
 from tensorflow import keras
@@ -22,24 +22,10 @@ kernel_init = keras.initializers.RandomNormal(mean=0.0, stddev=0.02)
 # Gamma initializer for instance normalization.
 gamma_init = keras.initializers.RandomNormal(mean=0.0, stddev=0.02)
 
-
 def normalize_img(img):
     img = tf.cast(img, dtype=tf.float32)
     # Map values in the range [-1, 1]
     return (img / 127.5) - 1.0
-
-def load_and_preprocess_train_image(path):
-    img = tf.io.read_file(path)
-    img = tf.image.decode_jpeg(img, channels=3)
-    # Random flip
-    img = tf.image.random_flip_left_right(img)
-    # Resize to the original size first
-    img = tf.image.resize(img, [*orig_img_size])
-    # Random crop to 256X256
-    img = tf.image.random_crop(img, size=[*input_img_size])
-    # Normalize the pixel values in the range [-1, 1]
-    img = normalize_img(img)
-    return img
 
 def load_and_preprocess_test_image(path):
     img = tf.io.read_file(path)
@@ -49,19 +35,8 @@ def load_and_preprocess_test_image(path):
     img = normalize_img(img)
     return img
 
-
 # Building blocks used in the CycleGAN generators and discriminators
 class ReflectionPadding2D(layers.Layer):
-    """Implements Reflection Padding as a layer.
-
-    Args:
-        padding(tuple): Amount of padding for the
-        spatial dimensions.
-
-    Returns:
-        A padded tensor with the same type as the input tensor.
-    """
-
     def __init__(self, padding=(1, 1), **kwargs):
         self.padding = tuple(padding)
         super(ReflectionPadding2D, self).__init__(**kwargs)
@@ -200,250 +175,16 @@ def get_resnet_generator(
     model = keras.models.Model(img_input, x, name=name)
     return model
 
-
-# Build the discriminators
-def get_discriminator(filters=64, kernel_initializer=kernel_init, num_downsampling=3, name=None):
-    img_input = layers.Input(shape=input_img_size, name=name + "_img_input")
-    x = layers.Conv2D(
-        filters,
-        (4, 4),
-        strides=(2, 2),
-        padding="same",
-        kernel_initializer=kernel_initializer,
-    )(img_input)
-    x = layers.LeakyReLU(0.2)(x)
-
-    num_filters = filters
-    for num_downsample_block in range(3):
-        num_filters *= 2
-        if num_downsample_block < 2:
-            x = downsample(
-                x,
-                filters=num_filters,
-                activation=layers.LeakyReLU(0.2),
-                kernel_size=(4, 4),
-                strides=(2, 2),
-            )
-        else:
-            x = downsample(
-                x,
-                filters=num_filters,
-                activation=layers.LeakyReLU(0.2),
-                kernel_size=(4, 4),
-                strides=(1, 1),
-            )
-
-    x = layers.Conv2D(
-        1, (4, 4), strides=(1, 1), padding="same", kernel_initializer=kernel_initializer
-    )(x)
-
-    model = keras.models.Model(inputs=img_input, outputs=x, name=name)
-    return model
-
-# Get the generators
-gen_G = get_resnet_generator(name="generator_G")
-gen_F = get_resnet_generator(name="generator_F")
-
-# Get the discriminators
-disc_X = get_discriminator(name="discriminator_X")
-disc_Y = get_discriminator(name="discriminator_Y")
-
-
-# Build the CycleGAN model
-class CycleGan(keras.Model):
-    def __init__(
-        self,
-        generator_G,
-        generator_F,
-        discriminator_X,
-        discriminator_Y,
-        lambda_cycle=10.0,
-        lambda_identity=0.5,
-    ):
-        super(CycleGan, self).__init__()
-        self.gen_G = generator_G
-        self.gen_F = generator_F
-        self.disc_X = discriminator_X
-        self.disc_Y = discriminator_Y
-        self.lambda_cycle = lambda_cycle
-        self.lambda_identity = lambda_identity
-
-    def compile(
-        self,
-        gen_G_optimizer,
-        gen_F_optimizer,
-        disc_X_optimizer,
-        disc_Y_optimizer,
-        gen_loss_fn,
-        disc_loss_fn,
-    ):
-        super(CycleGan, self).compile()
-        self.gen_G_optimizer = gen_G_optimizer
-        self.gen_F_optimizer = gen_F_optimizer
-        self.disc_X_optimizer = disc_X_optimizer
-        self.disc_Y_optimizer = disc_Y_optimizer
-        self.generator_loss_fn = gen_loss_fn
-        self.discriminator_loss_fn = disc_loss_fn
-        self.cycle_loss_fn = keras.losses.MeanAbsoluteError()
-        self.identity_loss_fn = keras.losses.MeanAbsoluteError()
-
-    def train_step(self, batch_data):
-        # x is crop and y is style
-        real_x, real_y = batch_data
-
-        # For CycleGAN, we need to calculate different
-        # kinds of losses for the generators and discriminators.
-        # We will perform the following steps here:
-        #
-        # 1. Pass real images through the generators and get the generated images
-        # 2. Pass the generated images back to the generators to check if we
-        #    we can predict the original image from the generated image.
-        # 3. Do an identity mapping of the real images using the generators.
-        # 4. Pass the generated images in 1) to the corresponding discriminators.
-        # 5. Calculate the generators total loss (adverserial + cycle + identity)
-        # 6. Calculate the discriminators loss
-        # 7. Update the weights of the generators
-        # 8. Update the weights of the discriminators
-        # 9. Return the losses in a dictionary
-
-        with tf.GradientTape(persistent=True) as tape:
-            # crop to style -> x to y
-            fake_y = self.gen_G(real_x, training=True)
-            # style to fake crop -> y to x
-            fake_x = self.gen_F(real_y, training=True)
-
-            # Cycle (crop to style to fake crop): x -> y -> x
-            cycled_x = self.gen_F(fake_y, training=True)
-            # Cycle (style to fake crop to fake style) y -> x -> y
-            cycled_y = self.gen_G(fake_x, training=True)
-
-            # Identity mapping
-            same_x = self.gen_F(real_x, training=True)
-            same_y = self.gen_G(real_y, training=True)
-
-            # Discriminator output
-            disc_real_x = self.disc_X(real_x, training=True)
-            disc_fake_x = self.disc_X(fake_x, training=True)
-
-            disc_real_y = self.disc_Y(real_y, training=True)
-            disc_fake_y = self.disc_Y(fake_y, training=True)
-
-            # Generator adverserial loss
-            gen_G_loss = self.generator_loss_fn(disc_fake_y)
-            gen_F_loss = self.generator_loss_fn(disc_fake_x)
-
-            # Generator cycle loss
-            cycle_loss_G = self.cycle_loss_fn(real_y, cycled_y) * self.lambda_cycle
-            cycle_loss_F = self.cycle_loss_fn(real_x, cycled_x) * self.lambda_cycle
-
-            # Generator identity loss
-            id_loss_G = (
-                self.identity_loss_fn(real_y, same_y)
-                * self.lambda_cycle
-                * self.lambda_identity
-            )
-            id_loss_F = (
-                self.identity_loss_fn(real_x, same_x)
-                * self.lambda_cycle
-                * self.lambda_identity
-            )
-
-            # Total generator loss
-            total_loss_G = gen_G_loss + cycle_loss_G + id_loss_G
-            total_loss_F = gen_F_loss + cycle_loss_F + id_loss_F
-
-            # Discriminator loss
-            disc_X_loss = self.discriminator_loss_fn(disc_real_x, disc_fake_x)
-            disc_Y_loss = self.discriminator_loss_fn(disc_real_y, disc_fake_y)
-
-        # Get the gradients for the generators
-        grads_G = tape.gradient(total_loss_G, self.gen_G.trainable_variables)
-        grads_F = tape.gradient(total_loss_F, self.gen_F.trainable_variables)
-
-        # Get the gradients for the discriminators
-        disc_X_grads = tape.gradient(disc_X_loss, self.disc_X.trainable_variables)
-        disc_Y_grads = tape.gradient(disc_Y_loss, self.disc_Y.trainable_variables)
-
-        # Update the weights of the generators
-        self.gen_G_optimizer.apply_gradients(
-            zip(grads_G, self.gen_G.trainable_variables)
-        )
-        self.gen_F_optimizer.apply_gradients(
-            zip(grads_F, self.gen_F.trainable_variables)
-        )
-
-        # Update the weights of the discriminators
-        self.disc_X_optimizer.apply_gradients(
-            zip(disc_X_grads, self.disc_X.trainable_variables)
-        )
-        self.disc_Y_optimizer.apply_gradients(
-            zip(disc_Y_grads, self.disc_Y.trainable_variables)
-        )
-
-        return {
-            "G_loss": total_loss_G,
-            "F_loss": total_loss_F,
-            "D_X_loss": disc_X_loss,
-            "D_Y_loss": disc_Y_loss,
-        }
-
-
-# Create the end-to-end model
-# Loss function for evaluating adversarial loss
-adv_loss_fn = keras.losses.MeanSquaredError()
-
-# Define the loss function for the generators
-def generator_loss_fn(fake):
-    fake_loss = adv_loss_fn(tf.ones_like(fake), fake)
-    return fake_loss
-
-# Define the loss function for the discriminators
-def discriminator_loss_fn(real, fake):
-    real_loss = adv_loss_fn(tf.ones_like(real), real)
-    fake_loss = adv_loss_fn(tf.zeros_like(fake), fake)
-    return (real_loss + fake_loss) * 0.5
-
-# Create cycle gan model
-cycle_gan_model = CycleGan(
-    generator_G=gen_G, 
-    generator_F=gen_F, 
-    discriminator_X=disc_X, 
-    discriminator_Y=disc_Y
-)
-
-# Compile the model
-cycle_gan_model.compile(
-    gen_G_optimizer=keras.optimizers.Adam(learning_rate=2e-4, beta_1=0.5),
-    gen_F_optimizer=keras.optimizers.Adam(learning_rate=2e-4, beta_1=0.5),
-    disc_X_optimizer=keras.optimizers.Adam(learning_rate=2e-4, beta_1=0.5),
-    disc_Y_optimizer=keras.optimizers.Adam(learning_rate=2e-4, beta_1=0.5),
-    gen_loss_fn=generator_loss_fn,
-    disc_loss_fn=discriminator_loss_fn,
-)
-
-"""
-# 暫時註解
-# cycle_gan_model.load_weights 載入模型會有錯誤
-# OSError: Unable to open file (file signature not found)"
-"""
-def load_model():
-    # Load the checkpoints
-    cycle_gan_model.built = True
-    cycle_gan_model.load_weights('app/ai/model/liGan.h5')
-    print("Model loaded")
-    return cycle_gan_model
-
-cg_model = None
-
-# Predict and transfer images
 def style_transfer(img_path):
-    global cg_model
-    if cg_model is None:
-        cg_model = load_model()
-
     img = load_and_preprocess_test_image(img_path)
     img = np.expand_dims(img, axis=0)
-    prediction = cg_model.gen_G(img, training=False)[0].numpy()
+    prediction = gen_G2(img, training=False)[0].numpy()
     prediction = (prediction * 127.5 + 127.5).astype(np.uint8)
     prediction = keras.preprocessing.image.array_to_img(prediction)
     return prediction
+
+gen_G = get_resnet_generator(name="generator_G")
+gen_G.load_weights('model/liGan_G.h5')
+
+prediction = style_transfer('0a855ad9-d9e8-40a6-8278-d86603b1db30.jpg')
+prediction.save("transfered.png")
